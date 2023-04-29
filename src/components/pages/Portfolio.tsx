91dadc603c472f07a7f645c22f3537c4b9acfb08
Soto <quickfire25@yahoo.com>
2023-04-29 15:04:43 -0500

    portfolio icons

--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -4,12 +4,14 @@ export default function Portfolio() {
     return (
         <div className="section container">
             <div className="sectionHeader row">
-            <h2>Portfolio</h2>
+                <h2>Portfolio</h2>
             </div>
             <div className="sectionBody row justify-content-center">
-            <p className="col-8 text-center">This is the section where I do a write up about me, 
+                <div className="col-2 glyphicon text-center">&#10094;</div>
+                <p className="col-8 text-center">This is the section where I do a write up about me, 
                 how I got here, and what I want to achieve.</p>
-                </div>
+                <div className="col-2 glyphicon text-center">&#10095;</div>
+            </div>
         </div>
     )
 }
