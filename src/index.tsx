d76cc552c7c9d828f1ca4cfbb802f1480811b70b
Soto <quickfire25@yahoo.com>
2023-04-29 12:15:22 -0500

    fixed homepage, rendering correct now

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { StrictMode } from 'react';
-import 'boostrap/dist/css/boostrap.min.css';
+// import 'boostrap/dist/css/boostrap.min.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
